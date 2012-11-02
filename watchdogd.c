a73c80616a706772b5e82f4d8ca459763a593f09
Joachim Nilsson <troglobit@gmail.com>
2012-11-02 17:35:12 +0100

    Support for forcing external kick, with optional startup delay
    This commit adds support for forcing an external kick, with an optional
    startup delay.  Very useful in setups where an external supervisor
    daemon is expected to use SIGUSR1 to watchdogd to kick the hardware
    watchdog.
    
    In this mode we expect there is an external daemon and if that daemon
    does not start and not even sends an initial SIGUSR1 we can catch that
    too.  The option supports a startup delay feature to work around startup
    phenomena to will and do occur.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- watchdogd.c
+++ watchdogd.c
@@ -2,6 +2,7 @@
  *
  * Copyright (C) 2008 Michele d'Amico <michele.damico@fitre.it>
  * Copyright (C) 2008 Mike Frysinger <vapier@gentoo.org>
+ * Copyright (C) 2012 Joachim Nilsson <troglobit@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -43,9 +44,11 @@
 #define INFO(fmt, args...)                    print(LOG_DAEMON | LOG_INFO,  fmt, ##args)
 
 int fd      = -1;
+int magic   = 0;
 int verbose = 0;
 int sys_log = 0;
 int extkick = 0;
+int extdelay = 0;
 extern char *__progname;
 
 int daemonize(char *output);
@@ -61,7 +64,7 @@ static void wdt_kick(char *msg)
 {
 	int dummy;
 
-	DEBUG("%s", msg ?: "Kicking watchdog.");
+	DEBUG("%s", msg);
 	ioctl(fd, WDIOC_KEEPALIVE, &dummy);
 }
 
@@ -89,11 +92,16 @@ static int wdt_get_timeout(void)
 	return count;
 }
 
-static void wdt_magic_close(int UNUSED(signo))
+static void wdt_close(int UNUSED(signo))
 {
 	if (fd != -1) {
-		DEBUG("Safe exit, disabling HW watchdog.");
-		write(fd, "V", 1);
+		/* When --safe-exit is selected */
+		if (magic) {
+			INFO("Safe exit, disabling HW watchdog.");
+			write(fd, "V", 1);
+		} else
+			INFO("Exiting, watchdog still active.");
+
 		close(fd);
 	}
 	exit(0);
@@ -101,11 +109,13 @@ static void wdt_magic_close(int UNUSED(signo))
 
 static void wdt_external_kick(int UNUSED(signo))
 {
-	if (!extkick)
+	if (!extkick) {
+		extdelay = 0;
+		extkick = 1;
 		INFO("External supervisor now controls watchdog kick via SIGUSR1.");
+	}
 
-	extkick = 1;
-	wdt_kick("External watchdog kick.");
+	wdt_kick("External kick.");
 }
 
 static void wdt_external_kick_exit(int UNUSED(signo))
@@ -114,19 +124,16 @@ static void wdt_external_kick_exit(int UNUSED(signo))
 	extkick = 0;
 }
 
-static void setup_signals(int magic)
+static void setup_signals(void)
 {
 	struct sigaction sa;
 
 	memset(&sa, 0, sizeof(sa));
 	sigemptyset(&sa.sa_mask);
 
-	/* When --safe-exit is selected */
-	if (magic) {
-		sa.sa_handler = wdt_magic_close;
-		sigaction(SIGINT, &sa, NULL);
-		sigaction(SIGTERM, &sa, NULL);
-	}
+	sa.sa_handler = wdt_close;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
 
 	/* Kick from external process supervisor */
 	sa.sa_handler = wdt_external_kick;
@@ -142,15 +149,17 @@ static int usage(int status)
 	printf("Usage: %s [-f] [-w <sec>] [-k <sec>] [-s] [-h|--help]\n"
                "A simple watchdog deamon that kicks /dev/watchdog every %d sec, by default.\n"
                "Options:\n"
-               "  --foreground, -f       Start in foreground (background is default)\n"
-	       "  --logfile, -l <file>   Log to <file> when backgrounding, otherwise silent\n"
-	       "  --syslog, -L           Use syslog, even if in foreground\n"
-               "  --timeout, -w <sec>    Set the HW watchdog timeout to <sec> seconds\n"
-               "  --interval, -k <sec>   Set watchdog kick interval to <sec> seconds\n"
-               "  --safe-exit, -s        Disable watchdog on exit from SIGINT/SIGTERM\n"
-	       "  --verbose, -V          Verbose operation, noisy output suitable for debugging\n"
-	       "  --version, -v          Display daemon version and exit\n"
-               "  --help, -h             Display this help message and exit\n",
+               "  --foreground, -f         Start in foreground (background is default)\n"
+	       "  --external-kick, -x [N]  Force external watchdog kick using SIGUSR1\n"
+	       "                           A 'N x <interval>' delay for startup is given\n"
+	       "  --logfile, -l <file>     Log to <file> when backgrounding, otherwise silent\n"
+	       "  --syslog, -L             Use syslog, even if in foreground\n"
+               "  --timeout, -w <sec>      Set the HW watchdog timeout to <sec> seconds\n"
+               "  --interval, -k <sec>     Set watchdog kick interval to <sec> seconds\n"
+               "  --safe-exit, -s          Disable watchdog on exit from SIGINT/SIGTERM\n"
+	       "  --verbose, -V            Verbose operation, noisy output suitable for debugging\n"
+	       "  --version, -v            Display version and exit\n"
+               "  --help, -h               Display this help message and exit\n",
                __progname, WDT_TIMEOUT_DEFAULT);
 
 	return status;
@@ -162,26 +171,33 @@ int main(int argc, char *argv[])
 	int real_timeout = 0;
 	int period = -1;
 	int background = 1;
-	int magic = 0;
 	int c;
 	char *logfile = NULL;
 	struct option long_options[] = {
-		{"foreground", 0, 0, 'f'},
-		{"logfile",    1, 0, 'l'},
-		{"syslog",     0, 0, 'L'},
-		{"timeout",    1, 0, 'w'},
-		{"interval",   1, 0, 'k'},
-		{"safe-exit",  0, 0, 's'},
-		{"verbose",    0, 0, 'V'},
-		{"version",    0, 0, 'v'},
-		{"help",       0, 0, 'h'},
+		{"foreground",    0, 0, 'f'},
+		{"external-kick", 2, 0, 'x'},
+		{"logfile",       1, 0, 'l'},
+		{"syslog",        0, 0, 'L'},
+		{"timeout",       1, 0, 'w'},
+		{"interval",      1, 0, 'k'},
+		{"safe-exit",     0, 0, 's'},
+		{"verbose",       0, 0, 'V'},
+		{"version",       0, 0, 'v'},
+		{"help",          0, 0, 'h'},
 		{NULL, 0, 0, 0}
 	};
 
-	while ((c = getopt_long(argc, argv, "fl:Lw:k:sVvh?", long_options, NULL)) != EOF) {
+	while ((c = getopt_long(argc, argv, "fx::l:Lw:k:sVvh?", long_options, NULL)) != EOF) {
 		switch (c) {
 		case 'f':	/* Run in foreground */
 			background = 0;
+			break;
+
+		case 'x':
+			if (!optarg)
+				extdelay = 1; /* Default is 1 x period */
+			else
+				extdelay = atoi(optarg);
 			break;
 
 		case 'l':	/* Log to file */
@@ -197,10 +213,18 @@ int main(int argc, char *argv[])
 			break;
 
 		case 'w':	/* Watchdog timeout */
+			if (!optarg) {
+				ERROR("Missing timeout argument.");
+				return usage(1);
+			}
 			timeout = atoi(optarg);
 			break;
 
 		case 'k':	/* Watchdog kick interval */
+			if (!optarg) {
+				ERROR("Missing interval argument.");
+				return usage(1);
+			}
 			period = atoi(optarg);
 			break;
 
@@ -241,7 +265,7 @@ int main(int argc, char *argv[])
 	INFO("Userspace watchdog daemon v%s starting ...", VERSION);
 
 	/* Setup callbacks for SIGUSR1 and, optionally, exit magic on SIGINT/SIGTERM */
-	setup_signals(magic);
+	setup_signals();
 
 	if (pidfile(NULL))
 		PERROR("Cannot create pidfile");
@@ -273,15 +297,23 @@ int main(int argc, char *argv[])
 	}
 	DEBUG("Watchdog kick interval set to %d sec.", period);
 
-	/* When an external supervisor once has started sending SIGUSR1
-	 * it fully assumes responsibility for kicking. No magic here. */
 	while (1) {
 		int rem;
 
+		/* When an external supervisor once has started sending SIGUSR1
+		 * it fully assumes responsibility for kicking. No magic here. */
 		if (!extkick)
-			wdt_kick(NULL);
+			wdt_kick("Kicking watchdog.");
 
-		/* Check if awaken by signal */
+		/* Startup delay before handing over to external kick.
+		 * Wait MAX:@extdelay number of built-in kicks, MIN:1 */
+		if (extdelay) {
+			DEBUG("Pending external kick in %d sec ...", extdelay * period);
+			if (!--extdelay)
+				extkick = 1;
+		}
+
+		/* Check remaining time, if awaken by signal */
 		rem = period;
 		do {
 			rem = sleep(rem);
