e43541221060d0034f573d49c31267c7b2282a49
Joachim Nilsson <troglobit@gmail.com>
2016-06-19 08:52:56 +0200

    Update copyright years
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- wdt.h
+++ wdt.h
@@ -1,8 +1,8 @@
-/* A small userspace watchdog daemon
+/* Advanced watchdog daemon
  *
  * Copyright (C) 2008       Michele d'Amico <michele.damico@fitre.it>
  * Copyright (C) 2008       Mike Frysinger <vapier@gentoo.org>
- * Copyright (C) 2012-2015  Joachim Nilsson <troglobit@gmail.com>
+ * Copyright (C) 2012-2016  Joachim Nilsson <troglobit@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
