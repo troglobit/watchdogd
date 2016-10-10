a5c6bf354f23d3ecfa7a30472ec466c43318587c
Joachim Nilsson <troglobit@gmail.com>
2016-10-10 02:22:24 +0200

    ex2: Use compat API and check reset reason
    Also, add forced reset check.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- examples/ex2.c
+++ examples/ex2.c
@@ -1,4 +1,4 @@
-/* Another wdog client example, press Ctrl-C to exit ...
+/* Another wdog client example, using compat API, press Ctrl-C to exit ...
  *
  * Copyright (c) 2016  Joachim Nilsson <troglobit@gmail.com>
  *
@@ -28,8 +28,8 @@ extern int   __wdog_testmode;
 
 int main(int argc, char *argv[])
 {
-	int id, i;
-	int ack = -1, dbg = 0;
+	int id, i, dbg = 0;
+	unsigned int ack = -1;
 	wdog_reason_t reason;
 
 	if (argc >= 2 && !strncmp(argv[1], "-V", 2))
@@ -41,13 +41,15 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	wdog_set_debug(dbg);
-	wdog_get_debug(&ack);
-	DEBUG("OK (debug:%d)!", ack);
+	wdog_get_debug(&id);
+	DEBUG("OK (debug:%d)!", id);
 
 	wdog_reboot_reason(&reason);
 	printf("Reset reason: %s\n", wdog_reboot_reason_str(&reason));
-	
-	id = wdog_pmon_subscribe(NULL, 2000, &ack);
+	printf("wid %d, label %s, cause: %d, counter: %d\n",
+	       reason.wid, reason.label, reason.cause, reason.counter);
+
+	id = wdog_subscribe(NULL, 2000, &ack);
 	if (id < 0) {
 		perror("Failed connecting to pmon");
 		return 1;
@@ -56,14 +58,16 @@ int main(int argc, char *argv[])
 	printf("Press Ctrl-C to exit.\n");
 	for (i = 0; i < 200; i++) {
 		DEBUG("Kicking ...");
-		if (wdog_pmon_kick(id, &ack))
+		if (wdog_kick(id, 2000, ack, &ack))
 			PERROR("Failed kicking");
 
 		sleep(1);
+		if (i == 30)
+			wdog_forced_reset("Terminal failure");
 	}
 
 	DEBUG("Exiting ...");
-	if (wdog_pmon_unsubscribe(id, ack)) {
+	if (wdog_unsubscribe(id, ack)) {
 		PERROR("Failed unsubscribe");
 		return 1;
 	}
