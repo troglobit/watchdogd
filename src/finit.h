7e8c63a9602d037da3c2523c3c5f505ad1d18228
Joachim Wiberg <troglobit@gmail.com>
2022-05-23 18:34:46 +0200

    Import stripped finit.h to probe PID 1 at runtime
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ src/finit.h
@@ -0,0 +1,35 @@
+/* Finit (PID 1) API
+ *
+ * Copyright (C) 2017-2020  Joachim Wiberg <troglobit@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef WDOG_FINIT_H_
+#define WDOG_FINIT_H_
+
+#define INIT_SOCKET             _PATH_VARRUN "finit/socket"
+#define INIT_MAGIC              0x03091969
+#define INIT_CMD_WDOG_HELLO     128  /* Watchdog register and hello */
+#define INIT_CMD_NACK           254
+#define INIT_CMD_ACK            255
+
+struct init_request {
+	int	magic;
+	int	cmd;
+	int	runlevel;
+	int	sleeptime;
+	char	data[368];
+};
+
+#endif /* WDOG_FINIT_H_ */
