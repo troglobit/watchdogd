3f400bae3f8b2df683b854576be2057fc912ea45
Joachim Wiberg <troglobit@gmail.com>
2023-12-19 08:57:37 +0100

    Extend libwdog API documentation
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/wdog.h
+++ src/wdog.h
@@ -15,6 +15,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+/** libwdog API
+ * @file wdog.h
+ *
+ */
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -25,37 +30,60 @@ extern "C"
 
 #include <time.h>
 
-/* Reset reason codes */
+/** Reset reason codes */
 typedef enum {
-	WDOG_SYSTEM_NONE = 0,	      /* After reset/power-on */
-	WDOG_SYSTEM_OK,		      /* Unused? */
-	WDOG_FAILED_SUBSCRIPTION,     /* Supervised process */
-	WDOG_FAILED_KICK,	      /* Supervised process */
-	WDOG_FAILED_UNSUBSCRIPTION,   /* Supervised process */
-	WDOG_FAILED_TO_MEET_DEADLINE, /* Supervised process */
-	WDOG_FORCED_RESET,	      /* Operator requested system reboot */
-	WDOG_FAILED_UNKNOWN,	      /* Likely, WDT timed out*/
-	WDOG_DESCRIPTOR_LEAK,	      /* filenr  pluing */
-	WDOG_MEMORY_LEAK,	      /* meminfo plugin */
-	WDOG_CPU_OVERLOAD,	      /* loadavg plugin */
+	WDOG_SYSTEM_NONE = 0,	      /**< After reset/power-on */
+	WDOG_SYSTEM_OK,		      /**< Unused? */
+	WDOG_FAILED_SUBSCRIPTION,     /**< Supervised process */
+	WDOG_FAILED_KICK,	      /**< Supervised process */
+	WDOG_FAILED_UNSUBSCRIPTION,   /**< Supervised process */
+	WDOG_FAILED_TO_MEET_DEADLINE, /**< Supervised process */
+	WDOG_FORCED_RESET,	      /**< Operator requested system reboot */
+	WDOG_FAILED_UNKNOWN,	      /**< Likely, WDT timed out*/
+	WDOG_DESCRIPTOR_LEAK,	      /**< filenr  pluing */
+	WDOG_MEMORY_LEAK,	      /**< meminfo plugin */
+	WDOG_CPU_OVERLOAD,	      /**< loadavg plugin */
 } wdog_code_t;
 
+/** Reset reason data */
 typedef struct
 {
-	unsigned int  counter;   /* Global reset counter, not per-reason */
-	unsigned int  wid;       /* Watchdog ID of process causing reset */
-	wdog_code_t   code;      /* Reset reason code, use wdog_reset_reason_str() */
-	unsigned int  enabled;   /* Unused, kept for compat. */
-	char          label[48]; /* Process name causing reset, or label */
-	struct tm     date;      /* Recorded time of reset */
+	unsigned int  counter;   /**< Global reset counter, not per-reason */
+	unsigned int  wid;       /**< Watchdog ID of process causing reset */
+	wdog_code_t   code;      /**< Reset reason code, use wdog_reset_reason_str() */
+	unsigned int  enabled;   /**< Unused, kept for compat. */
+	char          label[48]; /**< Process name causing reset, or label */
+	struct tm     date;      /**< Recorded time of reset */
 } wdog_reason_t;
 
-int   wdog_set_debug        (int enable);   /* Toggle debug loglevel in daemon */
-int   wdog_get_debug        (int *status);  /* Check if debug is enabled */
+/**
+ * Toggle debug messages in daemon.
+ * @param enable when non-zero, enables @c LOG_DEBUG syslog messages.
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_set_debug(int enable);
 
-int   wdog_set_loglevel     (char *level);
-char *wdog_get_loglevel     (void);
+/**
+ * Get daemon debug status.
+ * @param[out] status non-zero when eanbled, must not be @c NULL.
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_get_debug(int *status);
 
+/**
+ * Change daemon log level.
+ * @param level one of: none, err, info, notice, debug
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_set_loglevel(char *level);
+
+/**
+ * Get daemon log level.
+ * @return See wdog_set_loglevel()
+ */
+char *wdog_get_loglevel(void);
+
+/** @privatesection */
 int   wdog_enable           (int enable);   /* Attempt to temp. disable */
 int   wdog_status           (int *status);  /* Check if enabled */
 
@@ -63,12 +91,59 @@ int   wdog_failed           (wdog_code_t code, int pid, char *label, unsigned in
 
 int   wdog_reset            (int pid, char *label);
 int   wdog_reset_timeout    (int pid, char *label, unsigned int timeout);
-int   wdog_reset_counter    (unsigned int *counter);
-int   wdog_reset_reason     (wdog_reason_t *reason);
-int   wdog_reset_reason_raw (wdog_reason_t *reason);
-char *wdog_reset_reason_str (wdog_reason_t *reason);
-int   wdog_reset_reason_clr (void);
 
+/** @publicesection */
+
+/**
+ * Get system reset counter (updated on every watchdog reset, incl. reboots)
+ * @param[out] counter pointer to where to return counter, must not be @c NULL.
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_reset_counter(unsigned int *counter);
+
+/**
+ * Get reset reason.
+ *
+ * This function fecthes the reset reason data from the daemon.  The
+ * retrived data can then be sent to wdog_reset_reason_str() to get a
+ * human readable string.
+ *
+ * @param[out] reason pointer to where to return wdog_reason_t, must not be @c NULL.
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_reset_reason(wdog_reason_t *reason);
+
+/**
+ * Get reset reason (raw).
+ *
+ * Similar to wdog_reset_reason(), except this reads the prepared reset
+ * reason from disk, which will be used in case of sudden power loss.
+ *
+ * @param[out] reason pointer to where to return wdog_reason_t, must not be @c NULL.
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_reset_reason_raw(wdog_reason_t *reason);
+
+/**
+ * Translates wdog_code_t to human-readable string.
+ * @param reason pointer to reset reason data.
+ * @return always returns a constant string, even for @c NULL reason.
+ */
+char *wdog_reset_reason_str(wdog_reason_t *reason);
+
+/**
+ * Clear reset reason, including reset counter.
+ *
+ * Please note, in general you should never call this function.  There
+ * is no need for it and some fault cases cannot be detected afterwards.
+ * The function only exists for requirements mapping to the request of
+ * some customers.
+ *
+ * @return POSIX OK(0) on success, non-zero on error.
+ */
+int wdog_reset_reason_clr(void);
+
+/** @privatesection */
 /*
  * Check if watchdogd API is actively responding,
  * returns %TRUE(1) or %FALSE(0)
@@ -79,14 +154,15 @@ int   wdog_reload           (void);
 /*
  * Process supervisor API, see also compat.h
  */
+/** @publicesection */
 
 /**
  * Start supervising a subscriber
  *
  * After this, one of the kick functions must be called at least every
- * @param timeout millisecods until wdog_unsubscribe() is called.  If
- * not, watchdogd will (depending on the configuration) reset the system
- * or call the supervisor script.
+ * @p timeout millisecods until wdog_unsubscribe() is called.  If not,
+ * watchdogd will (depending on the configuration) reset the system or
+ * call the supervisor script.
  *
  * @param label Name of this subscriber. If @c NULL, process ID will be used.
  * @param timeout Timeout in milliseconds
@@ -109,7 +185,7 @@ int   wdog_unsubscribe      (int id, unsigned int ack);
 /**
  * Kick the watchdog with a custom timeout (old API)
  *
- * Checks ack, restarts timer with provided timeout and sets @param next_ack.
+ * Checks ack, resets timer with provided timeout and sets @param next_ack.
  * This API is kept for backwards compatibility.  The new wdog_kick2() API is
  * a lot easier to use.
  *
@@ -125,7 +201,7 @@ int   wdog_kick             (int id, unsigned int timeout, unsigned int ack, uns
 /**
  * Kick the watchdog with a custom timeout
  *
- * Checks ack, restarts timer with new timeout and sets ack.  Use this
+ * Checks ack, resets timer with new timeout and sets ack.  Use this
  * to extend the kick interval set in wdog_subscribe().
  *
  * @param id return value from wdog_subscribe
@@ -138,8 +214,8 @@ int   wdog_extend_kick      (int id, unsigned int timeout, unsigned int *ack);
 /**
  * Kick the watchdog
  *
- * Checks ack, restarts timer and sets next_ack
- * Uses the timeout value provided in wdog_subscribe
+ * Checks ack, resets timer and sets @p next_ack.  Uses the @p timeout value
+ * provided in wdog_subscribe().
  *
  * @param id The ID returned from wdog_subscribe()
  * @param[in,out] ack Pointer to ack received from last wdog API call.  Will be updated with new ack.
